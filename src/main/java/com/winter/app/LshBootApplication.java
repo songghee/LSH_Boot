c93d8d7a41b51ae227e95757e869a310007a08e4
songghee <hiya12love@naver.com>
2024-03-05 12:39:04 +0900

    초기 설정

---
+++ src/main/java/com/winter/app/LshBootApplication.java
@@ -0,0 +1,13 @@
+package com.winter.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LshBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LshBootApplication.class, args);
+	}
+
+}
